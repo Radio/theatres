c44e94109f83db7d274ed5a33d57ecd5f0042850
Max Gopey <max.gopey@symmetrics.de>
2014-03-28 20:33:30 +0200

    Security implemented.

--- app/app.php
+++ app/app.php
@@ -12,20 +12,8 @@ require_once __DIR__ . '/bootstrap.php';
 $app = new Silex\Application();
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 $app->register(new Silex\Provider\SessionServiceProvider());
-
 if ($secure) {
-    $app->register(new Silex\Provider\SecurityServiceProvider(), array(
-        'security.firewalls' => array(
-            'admin' => array(
-                'pattern' => '^/admin',
-                'http' => true,
-                'users' => array(
-                    // raw password is foo
-                    'admin' => array('ROLE_ADMIN', '5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg=='),
-                ),
-            ),
-        )
-    ));
+    require_once __DIR__ . '/security.php';
 }
 
 $app['debug'] = $debug;
